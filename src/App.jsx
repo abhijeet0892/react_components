a0d3db403f60c9a43f6892e3f633a6f638c35362
abhijeet0892 <abhijeetchougule1008@gmail.com>
2024-04-20 23:13:56 +0530

    adding parallax1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Parallax1 } from "./components";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
 
+import "./App.css";
+
+const routes = [
+  {
+    name: "Parallax1",
+    path: "/parallax/parallax-1",
+    element: <Parallax1 />,
+  },
+];
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+  },
+  ...routes,
+]);
+
+function Layout() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="app">
+      {routes.map((route) => (
+        <Link key={route.path} to={route.path}>
+          {route.name}
+        </Link>
+      ))}
+    </main>
+  );
 }
 
-export default App
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
